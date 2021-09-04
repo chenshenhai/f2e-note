154f7e67fde5a2859033796932898f318f9e5b6d
chenshenhai <cshenhai@gmail.com>
2021-09-04 17:03:34 +0800

    feat: update

--- demo/func/iframe-docsrc/index.js
+++ demo/func/iframe-docsrc/index.js
@@ -2,12 +2,11 @@ const body = document.querySelector('body');
 const docScript = document.querySelector('#doc-script').innerText;
 const docHTML = document.querySelector('#doc-html').innerHTML;
 
-
-console.log(docScript);
-console.log(docHTML);
-
-const iframe = document.createElement('iframe');
-
+const data = {
+  html: docHTML,
+  w: 400,
+  h: 320,
+}
 const iframeSrcDoc = `
 <html>
   <head>
@@ -27,9 +26,18 @@ const iframeSrcDoc = `
   </script>
 </html>
 `
+
+const iframe = document.createElement('iframe');
+iframe.width = data.w;
+iframe.height = data.h;
 iframe.srcdoc = iframeSrcDoc;
+iframe.sandbox = "allow-scripts"
 body.appendChild(iframe);
 
-console.log('Hello World')
+
+window.addEventListener('message', (data) => {
+  console.log('message: data ====', data);
+}, false)
+
 
 
