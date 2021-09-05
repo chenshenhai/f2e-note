cc52a477439f8561894e36e09f2730a9f984881c
chenshenhai <cshenhai@gmail.com>
2021-09-05 10:16:53 +0800

    feat: update

--- demo/func/iframe-docsrc/index.js
+++ demo/func/iframe-docsrc/index.js
@@ -3,10 +3,16 @@ const docScript = document.querySelector('#doc-script').innerText;
 const docHTML = document.querySelector('#doc-html').innerHTML;
 
 const data = {
-  html: docHTML,
-  w: 400,
+  w: 440,
   h: 320,
+  desc: {
+    html: docHTML,
+    width: 400,
+    height: 200, 
+  }
 }
+const scaleX = data.w / data.desc.width;
+const scaleY = data.h / data.desc.height;
 const iframeSrcDoc = `
 <html>
   <head>
@@ -18,7 +24,13 @@ const iframeSrcDoc = `
     </style>
   </head>
   <body>
-    ${docHTML}
+    <div id="content" 
+      style="
+      transform: 
+        scaleX(${data.w/data.desc.width}) scaleY(${data.h/data.desc.height}) translateX(${(data.w - data.desc.width) / scaleX / 2}px) translateY(${(data.h - data.desc.height) / scaleY / 2}px);
+    ">
+      ${docHTML}
+    </div>
   </body>
   <script>
   ${docScript}
@@ -28,6 +40,7 @@ const iframeSrcDoc = `
 `
 
 const iframe = document.createElement('iframe');
+iframe.style = 'margin-top: 100px; margin-left: 80px; border: 1px solid #c0c0c0;'
 iframe.width = data.w;
 iframe.height = data.h;
 iframe.srcdoc = iframeSrcDoc;
